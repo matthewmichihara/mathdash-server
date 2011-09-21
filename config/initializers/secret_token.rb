e13c0b320072290c1a7d102a4016854acfd37984
Matthew Michihara <matthew.michihara@gmail.com>
2011-09-21 00:14:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathdash::Application.config.secret_token = '2f7028fae5c7971d5065f58ebd43e55a63adc8a361e9c2ca73dc130aa202cb8175a30ad64be5b9648dc7c533079d1f7d2e41fe967085b6ad11d30bbadb95895f'
